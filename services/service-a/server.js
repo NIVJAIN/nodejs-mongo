81627ff5b6bdaa323c49d1e417963eabdcd79f79
sripal jain <sripal.jain@gmail.com>
2021-05-21 18:44:59 +0800

    k8s

--- services/service-a/server.js
+++ services/service-a/server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 const bodyParser = require('body-parser')
 const fs = require('fs');
+const axios = require('axios');
 
 
 app.get('/', (req,res,next)=>{
@@ -26,6 +27,42 @@ app.get('/ping', (req,res,next)=>{
     })
 })
 
+app.get("/service-b",(req,res,next)=>{
+    axios.get('http://service-b/health')
+    .then(response => {
+        console.log(response.data.url);
+        console.log(response.data.explanation);
+        res.status(200).json({
+            from: "serva-> call to service->b",
+            message: response.data,
+            anothermessage: response.data.explanation
+        })
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: error,
+        })
+    });   
+})
+app.get("/service-c",(req,res,next)=>{
+    axios.get('http://service-c/health')
+    .then(response => {
+        console.log(response.data.url);
+        console.log(response.data.explanation);
+        res.status(200).json({
+            from: "serva-> call to service->c",
+            message: response.data,
+            anothermessage: response.data.explanation
+        })
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: error,
+        })
+    });   
+})
 
 app.listen(PORT, function(){
     console.log(`magik happens on http://localhost:${PORT}`)
